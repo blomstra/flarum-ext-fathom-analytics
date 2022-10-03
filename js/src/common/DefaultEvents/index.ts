5b6c39e343f59720bfd42f10a7d55c731ef9d040
David Wheatley <hi@davwheat.dev>
2022-10-03 10:14:00 +0100

    fix: correctly export default event

--- js/src/common/DefaultEvents/index.ts
+++ js/src/common/DefaultEvents/index.ts
@@ -1,3 +1,4 @@
 export { default as OpenLogInModal } from './OpenLogInModal';
 export { default as OpenSignUpModal } from './OpenSignUpModal';
+export { default as ViewBlogIndexPage } from './ViewBlogIndexPage';
 export { default as ViewTagsPage } from './ViewTagsPage';
